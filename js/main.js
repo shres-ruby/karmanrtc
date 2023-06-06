0e1d290868b6696ec6e61a772ebabddb7d9fae2a
shres-ruby <shres.ruby@gmail.com>
2023-06-06 15:48:01 +0545

    preloader added and basic homepage design added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var preloader = document.getElementById("loader");
+
+
+window.addEventListener("load", function(){
+    this.setTimeout(function(){
+        preloader.style.display = "none";}, 2000)
+    
+})
