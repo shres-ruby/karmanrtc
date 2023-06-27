7b7897aceacf72c8d16d96e9dae6da0a59ef7a89
shres-ruby <shres.ruby@gmail.com>
2023-06-27 16:12:51 +0545

    added hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//logo spinner start
 var preloader = document.getElementById("loader");
 
 
@@ -5,4 +6,16 @@ window.addEventListener("load", function(){
     this.setTimeout(function(){
         preloader.style.display = "none";}, 2000)
     
-})
+});
+//logo spinner end
+
+
+
+//hamburger menu start
+const menu_btn = document.querySelector('.hamburger');
+const navmenu = document.querySelector('nav');
+window.addEventListener("click", function(){
+    menu_btn.classList.toggle('is-active');
+    navmenu.classList.toggle('is-active');
+});
+//hamburger menu end
